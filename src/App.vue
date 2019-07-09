23ab8bd25538bf2e1c25e9d837ec952ded0b016d
Josh <Parsons>
2019-07-09 01:55:36 -0400

    added new template to portfolio

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app dark>
+  <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+      temporary
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar class="blue-grey darken-4 mb-5" fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-layout justify-center row wrap class="mt-5">
+        <router-view></router-view>
+    </v-layout>
+  </v-app>
+</template>
+<script>
+  export default {
+    data: () => {
+      return {
+        drawer: false,
+      }
+    }
+  }
+  
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-carousel height="700" class="mt-3">
+      <v-carousel-item
+        v-for="(item,i) in items"
+        :key="i"
+        :src="item.src"
+      ></v-carousel-item>
+    </v-carousel>
+</template>
+
+<script>
+export default {
+  components: {
+    CoreFooter: () => import('@/components/core/Footer'),
+    CoreToolbar: () => import('@/components/core/Toolbar'),
+    CoreView: () => import('@/components/core/View')
+  },
+  data: () => {
+    return {
+      items: [
+          {
+            src: require('@/assets/header.jpg')
+          },
+          {
+            src: require('@/assets/carousel2.jpg')
+          },
+          {
+            src: require('@/assets/carousel3.jpg')
+          },
+          {
+            src: require('@/assets/carousel4.jpg')
+          }
+        ]
+    }
+  }
+}
+</script>
